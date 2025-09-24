417de03586f33dc3a09e9921f214a25deaa1ec36
qiulin <qiulin@smm.cn>
2025-09-24 12:56:35 +0800

    Refactor App component: remove unused elements, add QR code image, and update text for video playback.

--- src/App.tsx
+++ src/App.tsx
@@ -2,41 +2,20 @@
  * @Author: qiulin qiulin@smm.cn
  * @Date: 2025-09-24 10:49:59
  * @LastEditors: qiulin qiulin@smm.cn
- * @LastEditTime: 2025-09-24 12:21:12
+ * @LastEditTime: 2025-09-24 12:55:33
  * @FilePath: /lx-teaching-case/src/App.tsx
  * @Description: 
  */
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import logo from './assets/qrcode.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count1111 is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <p>扫描二维码播放视频</p>
+      <img src={logo}></img>
+    </div>
   )
 }
 
